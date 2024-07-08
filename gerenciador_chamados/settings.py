d5a53b23e18a9e79ec341940badb98999bca4c73
eduardo.sballico@gmail.com <eduardo.sballico@gmail.com>
2024-07-07 21:23:36 -0300

    Iniciando app clientes

--- gerenciador_chamados/settings.py
+++ gerenciador_chamados/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "chamados.apps.ChamadosConfig"
+    "chamados.apps.ChamadosConfig",
+    "clientes.apps.ClientesConfig",
 ]
 
 MIDDLEWARE = [
